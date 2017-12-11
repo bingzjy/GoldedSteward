a56683c000164e7b71600ecac3389c2f219b9c73
zhangjinye <1015384173@qq.com>
2017-12-11 17:36:50 +0800

    version-4.3.5

---
+++ app/src/main/java/com/ldnet/activity/MyTestActivity.java
@@ -0,0 +1,15 @@
+package com.ldnet.activity;
+
+/**
+ * @author zhangjinye
+ * @name GoldedSteward2
+ * @class name：com.ldnet.activity
+ * @class describe
+ * @time 2017/12/11 17:36
+ * @change
+ * @chang time
+ * @class describe
+ */
+
+public class MyTestActivity {
+}
